f7456895554a60c5b343b5cbbec628a940dbc6c8
KiszczixIsCoding <kamilkiszkozgierski@gmail.com>
2021-04-18 21:17:08 +0200

    Implement StatisticsGenerator

--- task1/code/ksr.zad1/Classifier/src/main/java/pl/ksr/pon/cla/StatisticsGenerator.java
+++ task1/code/ksr.zad1/Classifier/src/main/java/pl/ksr/pon/cla/StatisticsGenerator.java
@@ -1,16 +1,67 @@
 package pl.ksr.pon.cla;
 
 import pl.ksr.pon.ext.Article;
-
+import pl.ksr.pon.ext.ClassifiedPlaces;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StatisticsGenerator {
-    double accuracy;
-    double precision;
-    double recall;
-    double f1;
 
-    public void generateStatistics(List<Article> testingList) {
+    public Map<String, Integer> generateRatesMap(ClassifiedPlaces place, List<Article> testingList) {
 
+        Map<String, Integer> ratesMap = new HashMap<>();
+        ratesMap.put("truePositive", 0);
+        ratesMap.put("falsePositive", 0);
+        ratesMap.put("falseNegative", 0);
+        ratesMap.put("trueNegative", 0);
+
+
+        for (Article article : testingList) {
+            if (article.getPlace() == place && article.getPredictedPlace() == place) {
+                ratesMap.put("truePositive", ratesMap.get("truePositive") + 1);
+            }
+            if (article.getPlace() == place && article.getPredictedPlace() != place) {
+                ratesMap.put("falsePositive", ratesMap.get("falsePositive") + 1);
+            }
+            if (article.getPlace() != place && article.getPredictedPlace() == place) {
+                ratesMap.put("falseNegative", ratesMap.get("falseNegative") + 1);
+            }
+            if (article.getPlace() != place && article.getPredictedPlace() != place) {
+                ratesMap.put("trueNegative", ratesMap.get("trueNegative") + 1);
+            }
+        }
+
+        return ratesMap;
+    }
+
+    public double countAccuracy(ClassifiedPlaces place, List<Article> testingList) {
+        Map<String, Integer> ratesMap = generateRatesMap(place, testingList);
+
+        double truePositive = (double)ratesMap.get("truePositive");
+        double trueNegative = (double)ratesMap.get("trueNegative");
+
+        return (truePositive + trueNegative) / (double)testingList.size();
+    }
+
+    public double countPrecision(ClassifiedPlaces place, List<Article> testingList) {
+        Map<String, Integer> ratesMap = generateRatesMap(place, testingList);
+        double truePositive = (double)ratesMap.get("truePositive");
+        double falsePositive = (double)ratesMap.get("falsePositive");
+
+        return truePositive / (truePositive + falsePositive);
+    }
+
+    public double countRecall(ClassifiedPlaces place, List<Article> testingList) {
+        Map<String, Integer> ratesMap = generateRatesMap(place, testingList);
+        double truePositive = (double)ratesMap.get("truePositive");
+        double falseNegative = (double)ratesMap.get("falseNegative");
+
+        return truePositive / (truePositive + falseNegative);
+    }
+
+    public double countF1(ClassifiedPlaces place, List<Article> testingList) {
+        return 2 * countPrecision(place, testingList) * countRecall(place, testingList)
+                / (countPrecision(place, testingList) + countRecall(place, testingList));
     }
 }
